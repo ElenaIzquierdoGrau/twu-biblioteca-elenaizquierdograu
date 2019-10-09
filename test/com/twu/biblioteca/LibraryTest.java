c2567570ff38b37324c292e9297c706264116c2e
Elena Izquierdo Grau <elenaizquierdograu@MacBook-Pro-de-Elena.local>
2019-10-09 10:27:15 +0200

    Refactor Library tests and changed test names

---
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -0,0 +1,133 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    private Library l = new Library();
+    @Test
+    public void successfullyListAllBooksAvailable(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        StringWriter expectedStringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(expectedStringWriter);
+        printWriter.println("Here you have the list of available books of the library");
+        printWriter.println("|      Title            |       Author      |   Year  |");
+        printWriter.println("Lo que el viento se llevo | Elena Izquierdo | 1997");
+        printWriter.println("La sombra del viento | Elena Grau | 2017");
+        String expected = expectedStringWriter.toString();
+
+        l.listAvailableBooks();
+
+        assertEquals(expected,outContent.toString());
+    }
+
+    @Test
+    public void getBookInfoCorrectlyWhenAskedByTitle(){
+        Book b = new Book("Lo que el viento se llevo",1997,"Elena Izquierdo");
+        Book res = l.infoBook("Lo que el viento se llevo");
+
+        assertEquals(b.title,res.title);
+        assertEquals(b.author,res.author);
+        assertEquals(b.year,res.year);
+    }
+
+    @Test
+    public void successfulCheckOutBookWhenCheckoutABookAvailable(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        l.checkOutBook("Lo que el viento se llevo");
+
+        StringWriter expectedStringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(expectedStringWriter);
+        printWriter.println("Thank you! Enjoy the book");
+        String expected = expectedStringWriter.toString();
+
+        assertEquals(expected,outContent.toString());
+    }
+
+    @Test
+    public void unsuccesfulCheckoutBookWhenWrongSpellingTitle(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        l.checkOutBook("Lo que el viento se levo");
+
+        StringWriter expectedStringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(expectedStringWriter);
+        printWriter.println("Sorry, that book is not available");
+        String expected = expectedStringWriter.toString();
+
+        assertEquals(expected,outContent.toString());
+    }
+
+    @Test
+    public void unsuccesfulCheckoutBookWhenBookAlreadyCheckedOut(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Book b = l.infoBook("Lo que el viento se llevo");
+        b.setCheckedOut(true);
+
+        l.checkOutBook("Lo que el viento se llevo");
+
+        StringWriter expectedStringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(expectedStringWriter);
+        printWriter.println("Sorry, that book is not available");
+        String expected = expectedStringWriter.toString();
+
+        assertEquals(expected,outContent.toString());
+    }
+
+    @Test
+    public void successfulReturnBookWhenBookIsAlreadyCheckedout(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        Book b = l.infoBook("Lo que el viento se llevo");
+        b.setCheckedOut(true);
+        l.returnBook("Lo que el viento se llevo");
+
+        StringWriter expectedStringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(expectedStringWriter);
+        printWriter.println("Thank you for returning the book");
+        String expected = expectedStringWriter.toString();
+
+        assertEquals(expected,outContent.toString());
+    }
+
+    @Test
+    public void unsuccessfulReturnBookWhenABookIsNotCheckedout(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        l.returnBook("Lo que el viento se llevo");
+
+        StringWriter expectedStringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(expectedStringWriter);
+        printWriter.println("That is not a valid book to return");
+        String expected = expectedStringWriter.toString();
+
+        assertEquals(expected,outContent.toString());
+    }
+
+    @Test
+    public void unsuccessfulReturnBookWhenSpellingTitleWrong(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        Book b = l.infoBook("Lo que el viento se llevo");
+        b.setCheckedOut(true);
+        l.returnBook("Lo que el veinto se llevo");
+
+        StringWriter expectedStringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(expectedStringWriter);
+        printWriter.println("That is not a valid book to return");
+        String expected = expectedStringWriter.toString();
+
+        assertEquals(expected,outContent.toString());
+    }
+
+}
